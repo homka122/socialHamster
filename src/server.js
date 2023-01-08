7a1a4a71e5fb0c91cc578e0b35c32702ddd2dcaa
Homka122 <kombaev02@gmail.com>
2023-01-08 10:21:46 +0500

    Setup for development

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app.js";
+import devSetup from "./devSetup.js";
 
 const start = async () => {
   mongoose.set('strictQuery', false)
   await mongoose.connect(process.env.DB_URL)
   await mongoose.connection.dropDatabase();
+  await devSetup();
+
   console.log("DB: OK")
 
   const PORT = process.env.PORT
