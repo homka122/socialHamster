7c357670ad39f4c1783c2785afa27287cb0a5890
Homka122 <kombaev02@gmail.com>
2023-01-16 09:14:57 +0500

    ApiError implementation

--- src/middleware/isLogin.js
+++ src/middleware/isLogin.js
@@ -1,23 +1,24 @@
 import authService from "../service/auth.js";
 import UserRepository from "../models/user.js";
+import { ApiError } from "../utils/ApiError.js"
 
 export const isLogin = async (req, res, next) => {
   const { authorization } = req.headers;
   const accessToken = authorization?.split(' ')[1]
 
   if (!accessToken) {
-    return next(new Error('Пользователь не авторизован'))
+    return next(new ApiError('Пользователь не авторизован', 403))
   }
 
   const decodedAccessToken = authService.verifyAccessToken(accessToken)
   if (!decodedAccessToken) {
-    return next(new Error('Невалидный access token'))
+    return next(new ApiError('Невалидный access token', 403))
   }
 
   const { username } = decodedAccessToken
   const user = await UserRepository.findOne({ username })
   if (!user) {
-    return next(new Error('Пользователя с таким access token нет.'))
+    return next(new ApiError('Пользователя с таким access token нет.', 403))
   }
 
   req.user = user;
