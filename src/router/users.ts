023a5477a0644ae384af6aa50639d79bff181573
Homka122 <kombaev02@gmail.com>
2023-02-03 10:19:16 +0500

    TS support for routers

--- src/router/users.ts
+++ src/router/users.ts
@@ -7,16 +7,11 @@ import { roleCheck } from '../middleware/roleCheck.js';
 const router = Router();
 
 router.use(isLogin);
-
 router.patch('/updatePhoto', uploadAvatar.single('avatar'), usersController.updatePhoto);
-router.get('/getPhoto', usersController.getPhoto);
-
 router.route('/:id').get(usersController.getUserInfo);
 
 router.use(roleCheck('ADMIN'));
-
 router.route('/').get(usersController.getAll).post(usersController.createOne);
-
 router.route('/:id').get(usersController.getOne).patch(usersController.updateOne).delete(usersController.deleteOne);
 
 export default router;
