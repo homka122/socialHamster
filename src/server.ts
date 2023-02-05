60ca34f4399f8711d1342c9650fddb93bf94d0f2
Homka122 <kombaev02@gmail.com>
2023-02-05 09:24:03 +0500

    Delete DevSetup

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
 import app from './app';
-import devSetup from './devSetup';
 import { createWebsocketServer, websocketEmitter } from './websocketApp';
 
 const start = async () => {
   mongoose.set('strictQuery', false);
   await mongoose.connect(process.env.DB_URL!);
-  await mongoose.connection.dropDatabase();
-  await devSetup();
 
   console.log('DB: OK');
 
